42907fd671167fa9eaca760186d818a234f98c3c
Scott Langridge <scott@derossi.consulting>
2022-06-02 15:40:18 +0100

    implement upgrading of room controller

--- src/subcontrollers/creep/starter.py
+++ src/subcontrollers/creep/starter.py
@@ -3,35 +3,38 @@ from ...subcontrollers.creep.creep import Creep
 
 
 class Starter(Creep):
-    SPAWN_CAP = 10
+    SPAWN_CAP = 9
     DEFAULT_BODY = [MOVE, MOVE, WORK, CARRY, CARRY]
-    DEFAULT_MEMORY = {'collecting': True}
+    DEFAULT_MEMORY = {'mining': True}
 
     def __init__(self, name):
         super().__init__(name)
 
     def run(self):
-        if self.memory.collecting:
+        self.decide_task()
+        if self.memory.mining:
             self.collect()
         else:
             self.deposit()
 
+    def decide_task(self):
+        print(self.name)
+        if self.empty() and not self.memory.mining:
+            self.obj.say('Mining')
+            self.memory.mining = True
+        elif self.full() and self.memory.mining:
+            self.obj.say('Depositing')
+            self.memory.mining = False
+
     def collect(self):
         target = self.pos.findClosestByPath(FIND_SOURCES_ACTIVE)
         code = self.obj.harvest(target)
-        if code == OK:
-            if self.obj.store.getFreeCapacity() == 0:
-                self.obj.say('Dropping')
-                self.memory.collecting = False
         if code == ERR_NOT_IN_RANGE:
             self.obj.moveTo(target)
 
     def deposit(self):
         target = self.get_target()
         code = self.obj.transfer(target, RESOURCE_ENERGY)
-        if code == OK:
-            self.obj.say('Collecting')
-            self.memory.collecting = True
         if code == ERR_NOT_IN_RANGE:
             self.obj.moveTo(target)
 
@@ -42,3 +45,9 @@ class Starter(Creep):
             return spawn
         else:
             return controller
+
+    def full(self):
+        return self.obj.store.getFreeCapacity() == 0
+
+    def empty(self):
+        return self.obj.store.getUsedCapacity() == 0
